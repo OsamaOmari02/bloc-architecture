519aba747bfb89b4a35eeac994a7a6fe4268332a
osama omari <osama.omarii02@gmail.com>
2023-02-27 02:55:50 +0300

    init commit

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,135 @@
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DioClient {
+  final Dio _dio = Dio();
+
+  DioClient() {
+    // _dio
+    //   ..options.baseUrl = APIEndPoints.baseUrl
+    //   ..options.connectTimeout = APIEndPoints.connectionTimeout
+    //   ..options.receiveTimeout = APIEndPoints.receiveTimeout
+    //   ..options.responseType = ResponseType.json
+    //   ..interceptors.add(TokenInterceptor());
+  }
+
+  Dio get dio => _dio;
+
+  // Get:-----------------------------------------------------------------------
+  Future<Response> get(
+      String url, {
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.get(
+        url,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Delete:--------------------------------------------------------------------
+  Future<dynamic> delete(
+      String url, {
+        data,
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onSendProgress,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.delete(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+      return response.data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Post:----------------------------------------------------------------------
+  Future<Response> post(
+      String url, {
+        data,
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onSendProgress,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.post(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Put:-----------------------------------------------------------------------
+  Future<Response> put(
+      String url, {
+        data,
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onSendProgress,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.put(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<dynamic> patch(String path,
+      [Object? requestBody,
+        Map<String, dynamic> queryParams = const {}]) async {
+    dynamic responseJson;
+    try {
+      // final response =
+      // await _dio.patch(APIEndPoints.baseUrl + path, data: requestBody);
+      // responseJson = _returnResponse(response as http.Response);
+    } on DioError catch (e) {
+      // final errorMessage = DioException.fromDioError(e).toString();
+      // throw errorMessage;
+    }
+    return responseJson;
+  }
+
+  dynamic _returnResponse(http.Response response) {
+    return json.decode(response.body.toString());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:your_parts/views/pages/my_home_page.dart';
+import 'package:your_parts/views/shared/themes/dark_theme.dart';
+import 'package:your_parts/views/shared/themes/light_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // init methods
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
+      home: const MyHomePage(),
+    );
+  }
+}
+
